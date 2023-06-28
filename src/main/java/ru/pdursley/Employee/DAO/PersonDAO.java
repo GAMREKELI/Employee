caaa156767bd48db26663101bea4f66629c44dbe
Никита Гамрекели <pdursley@MacBook-Pro-Nikita.local>
2023-06-28 21:52:52 +0300

    fix git from intellij

--- src/main/java/ru/pdursley/Employee/DAO/PersonDAO.java
+++ src/main/java/ru/pdursley/Employee/DAO/PersonDAO.java
@@ -103,8 +103,5 @@ public class PersonDAO {
 
         String personSQL = "UPDATE Person SET login = ?, password = ? WHERE id = ?";
         jdbcTemplate.update(personSQL, permission.getPerson().getLogin(), permission.getPerson().getPassword(), id);
-
-
-        
     }
 }
